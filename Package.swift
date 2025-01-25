01e26146a75da926c921c165cbba64be1ed57f81
yechentide <yechentide@gmail.com>
2025-01-25 16:37:27 +0900

    init

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "EasySync",
+    platforms: [
+        .macOS(.v13),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+        .package(url: "https://github.com/onevcat/Rainbow", .upToNextMajor(from: "4.1.0")),
+    ],
+    targets: [
+        .executableTarget(
+            name: "easy-sync",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "Rainbow", package: "Rainbow"),
+            ]
+        ),
+    ]
+)
---
+++ Sources/EasySync.swift
@@ -0,0 +1,23 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+// 
+// Swift Argument Parser
+// https://swiftpackageindex.com/apple/swift-argument-parser/documentation
+
+import ArgumentParser
+import Rainbow
+import Spinner
+import os
+import SwiftUI
+
+@main
+struct EasySync: ParsableCommand {
+    static let configuration = CommandConfiguration(
+        commandName: "easy-sync",
+        discussion: "",
+        version: "0.0.1",
+        shouldDisplay: true,
+        subcommands: [
+        ]
+    )
+}
